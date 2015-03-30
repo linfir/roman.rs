6d06b44e99f0db8cc915309235c67568bf86728b
Yann Villessuzanne <yann@miomio.fr>
2015-03-30 09:15:26 +0200

    Working roman numeral parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,33 @@
+//! Conversion between integers and roman numerals.
+
 #![cfg_attr(test,feature(str_words))]
 
-static ROMAN: [(&'static str, i32); 13] = [
-    ("M",  1000),
-    ("CM", 900),
-    ("D",  500),
-    ("CD", 400),
-    ("C",  100),
-    ("XC", 90),
-    ("L",  50),
-    ("XL", 40),
-    ("X",  10),
-    ("IX", 9),
-    ("V",  5),
-    ("IV", 4),
-    ("I",  1) ];
+static ROMAN: [(char, i32); 7] = [
+    ('I', 1), ('V', 5), ('X', 10), ('L', 50), ('C', 100), ('D', 500), ('M', 1000) ];
+static ROMAN_PAIRS: [(&'static str, i32); 13] = [
+    ("M", 1000), ("CM", 900), ("D",  500), ("CD", 400),
+    ("C", 100),  ("XC", 90),  ("L",  50),  ("XL", 40),
+    ("X", 10),   ("IX", 9),   ("V",  5),   ("IV", 4), ("I",  1) ];
 
 /// The largest number representable as a roman numeral.
-pub static MAX: i32 = 4999;
+pub static MAX: i32 = 3999;
 
 /// Converts an integer into a roman numeral.
 ///
-/// Works for integer between 1 and 4999 inclusive, returns None otherwise.
+/// Works for integer between 1 and 3999 inclusive, returns None otherwise.
 ///
 /// # Example
 ///
 /// ```
-/// use roman;
-///
 /// let x = roman::to(14);
 /// assert_eq!(x.unwrap(), "XIV");
 /// ```
 ///
 pub fn to(n: i32) -> Option<String> {
-    if n <= 0 || n >= 5000 { return None }
+    if n <= 0 || n > MAX { return None }
     let mut out = String::new();
     let mut n = n;
-    for i in ROMAN.iter() {
-        let (name, value) = *i;
+    for &(name, value) in ROMAN_PAIRS.iter() {
         while n >= value {
             n -= value;
             out.push_str(name);
@@ -54,4 +45,54 @@ fn test_to_roman() {
         assert_eq!(to(n).unwrap(), x);
     }
     assert_eq!(to(1984).unwrap(), "MCMLXXXIV");
+}
+
+/// Converts a roman numeral to an integer.
+///
+/// Works for integer between 1 and 3999 inclusive, returns None otherwise.
+///
+/// # Example
+///
+/// ```
+/// let x = roman::from("XIV");
+/// assert_eq!(x.unwrap(), 14);
+/// ```
+///
+pub fn from(txt: &str) -> Option<i32> {
+    let n = match from_lax(txt) {
+        Some(n) => n,
+        None    => return None
+    };
+    match to(n) {
+        Some(ref x) if *x == txt => Some(n),
+        _ => None
+    }
+}
+
+fn from_lax(txt: &str) -> Option<i32> {
+    let (mut n, mut max) = (0, 0);
+    for c in txt.chars().rev() {
+        let it = ROMAN.iter().find(|x| { let &(ch, _) = *x; ch == c } );
+        if it.is_none() { return None }
+        let &(_, val) = it.unwrap();
+        if val < max {
+            n -= val;
+        } else {
+            n += val;
+            max = val;
+        }
+    }
+    Some(n)
+}
+
+#[test]
+fn test_from() {
+    assert!(from("i").is_none());
+}
+
+#[test]
+fn test_to_from() {
+    for n in 1..MAX {
+        assert_eq!(from(&to(n).unwrap()).unwrap(), n);
+    }
 }
