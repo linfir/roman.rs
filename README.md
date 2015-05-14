2b965f0c4a086ebb914a104eeb612f63bd7d87ed
Yann Villessuzanne <yann@miomio.fr>
2015-05-14 12:31:23 +0200

    Minor changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![Build Status](https://travis-ci.org/linfir/roman.rs.svg?branch=master)](https://travis-ci.org/linfir/roman.rs)
 [![](https://meritbadge.herokuapp.com/roman)](https://crates.io/crates/roman)
-# roman
+# roman.rs
 
 A Rust library to convert between integers and roman numerals.
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 //! Conversion between integers and roman numerals.
 
-static ROMAN: [(char, i32); 7] = [
+static ROMAN: &'static [(char, i32)] = &[
     ('I', 1), ('V', 5), ('X', 10), ('L', 50), ('C', 100), ('D', 500), ('M', 1000) ];
-static ROMAN_PAIRS: [(&'static str, i32); 13] = [
+static ROMAN_PAIRS: &'static [(&'static str, i32)] = &[
     ("M", 1000), ("CM", 900), ("D",  500), ("CD", 400),
     ("C", 100),  ("XC", 90),  ("L",  50),  ("XL", 40),
-    ("X", 10),   ("IX", 9),   ("V",  5),   ("IV", 4), ("I",  1) ];
+    ("X", 10),   ("IX", 9),   ("V",  5),   ("IV", 4),
+    ("I", 1) ];
 
 /// The largest number representable as a roman numeral.
 pub static MAX: i32 = 3999;
